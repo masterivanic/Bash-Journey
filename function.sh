12d7751e0ec069aeef1db3fae413e204cd627b9d
Ivanic <62161915+masterivanic@users.noreply.github.com>
2025-12-23 02:50:08 +0100

    chore: add debugContainer function for Docker container info

--- function.sh
+++ function.sh
@@ -18,4 +18,15 @@ function hello() {
     echo "Hello world $1"
 }
 
+function debugContainer() {
+    read -p "Enter your containerName ? " containerName
+    echo "container name is: $containerName"
+    containerId=$(docker ps -a --format '{{.ID}}' -f "name=${containerName}" -n 1)
+    echo "container id is: $containerId"
+    PID=$(docker inspect -f '{{.State.Pid}}' ${containerName})
+    echo "container pid is: $PID"
+    docker run --rm --pid=host --net=host --privileged ubuntu bash -lc "nsenter -t $PID -m -u -i -n -p"
+}
+
+
 hello Dave
